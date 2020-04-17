5493777cd59f3b361b940cdaa437f750cf62b39c
João Nunes <j0a0nunes@hotmail.com>
2020-04-17 11:08:30 +0100

    no work

--- copy.py
+++ copy.py
@@ -1,12 +1,19 @@
 import shutil
+from pathlib import Path
+import os.path
+
+ignoredir = input("Path to ignore: ")
+newdir = input("Path to add: ")
 
 path = open("path.txt")
-dst = 'folderB/'
 dirs = []
 for line in path:
     dirs.append(line.strip())
 
 for n in dirs:
+    dst = n.replace(ignoredir, newdir)
+    dst = dst.rstrip('\\')
+    os.mkdir(dst)
     shutil.copy(n, dst)
 
 path.close()
